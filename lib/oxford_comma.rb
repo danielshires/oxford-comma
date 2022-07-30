9e385566dff2c13f5aab2e8ba57aef278a35b1ec
danielshires <hello@danielshires.com>
2022-07-30 14:46:45 +1000

    Assignment complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2 
+        array.join(" and ")
+    elsif array.length == 3
+        newArray = array.join(", ")
+        newArray.insert((newArray.length - array.last.length), "and ")
+    elsif array.length >= 4
+        newArray = array.join(", ")
+        newArray.insert((newArray.length - array.last.length), "and ")
+    end
 end
+
+array = ["fiddleheads","okra","kohlrabi"]
+
+puts oxford_comma(array).inspect
+
